42532a505c041138c1176c37b771eed52a006fad
pkafedjiski <pkafedjiski@travizer.com>
2024-12-03 14:19:46 +0200

    more branding

--- app/options/components/LinkBar.vue
+++ app/options/components/LinkBar.vue
@@ -2,23 +2,19 @@
     <b-navbar>
         <template slot="brand">
             <b-navbar-item :to="{ path: '/' }">
-                SHORTKEYS
+                NETSUITE SHORTCUTS
             </b-navbar-item>
         </template>
         <template slot="start">
-            <b-navbar-item href="https://chrome.google.com/webstore/detail/shortkeys-custom-keyboard/logpjaacgmcbpdkdchjiaagddngobkck/reviews?hl=en-US&gl=US">
-                Review
+            <b-navbar-item href="mailto:mdimkov@gmail.com">
+              Contact Developer: mdimkov@gmail.com
             </b-navbar-item>
-            <b-navbar-item href="https://github.com/mikecrittenden/shortkeys/wiki/How-To-Use-Shortkeys">
-                Documentation
-            </b-navbar-item>
-            <b-navbar-item href="https://github.com/mikecrittenden/shortkeys/issues">
-                Support
-            </b-navbar-item>
-            <b-navbar-item href="https://github.com/mikecrittenden/shortkeys">
-                GitHub
+            <b-navbar-item href="https://www.travizer.com/netsuite-keyboard-shortcuts.html">
+                Documentation and Demo
             </b-navbar-item>
         </template>
     </b-navbar>
 </template>
 
+<script lang="ts">
+</script>
